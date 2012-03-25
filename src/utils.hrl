60a938890a2a6974edee8f82e18b4f774daedd76
Guillaume Bour <guillaume@bour.cc>
2012-03-25 19:00:18 +0100

    [refactor] alignment

--- src/utils.hrl
+++ src/utils.hrl
@@ -1,11 +1,11 @@
 
 -define(DEBUG(Fmt, Args), 
-	logging:log(debug, lists:concat(["(", ?MODULE, ":", ?LINE, "):: ", Fmt, "~n"]), Args)).
+	logging:log(debug  , lists:concat(["(", ?MODULE, ":", ?LINE, "):: ", Fmt, "~n"]), Args)).
 -define(INFO(Fmt, Args), 
-	logging:log(info, lists:concat(["(", ?MODULE, ":", ?LINE, "):: ", Fmt, "~n"]), Args)).
+	logging:log(info   , lists:concat(["(", ?MODULE, ":", ?LINE, "):: ", Fmt, "~n"]), Args)).
 -define(NOTICE(Fmt, Args), 
-	logging:log(notice, lists:concat(["(", ?MODULE, ":", ?LINE, "):: ", Fmt, "~n"]), Args)).
+	logging:log(notice , lists:concat(["(", ?MODULE, ":", ?LINE, "):: ", Fmt, "~n"]), Args)).
 -define(WARNING(Fmt, Args), 
 	logging:log(warning, lists:concat(["(", ?MODULE, ":", ?LINE, "):: ", Fmt, "~n"]), Args)).
 -define(ERROR(Fmt, Args), 
-	logging:log(error, lists:concat(["(", ?MODULE, ":", ?LINE, "):: ", Fmt, "~n"]), Args)).
+	logging:log(error  , lists:concat(["(", ?MODULE, ":", ?LINE, "):: ", Fmt, "~n"]), Args)).
