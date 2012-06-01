00f331e620570d2adbb07644c32499cfac55217b
Guillaume Bour <guillaume@bour.cc>
2012-06-01 23:28:28 +0200

    [test] factorise authent modules tests

--- test/modules/mod_authent_internal_tests.erl
+++ test/modules/mod_authent_internal_tests.erl
@@ -3,9 +3,9 @@
 -include_lib("eunit/include/eunit.hrl").
 -include("sips/epbxd_sip.hrl").
 
--export([init_internal/0, down_internal/0]).
+-export([up/0, down/0]).
 
-init_internal() ->
+up() ->
     meck:new(mnesia),
     meck:expect(mnesia, dirty_read, fun(endpoints, Key) ->
         case Key of
@@ -14,55 +14,9 @@ init_internal() ->
         end
     end).
 
-down_internal() ->
+down() ->
     meck:unload(mnesia).
 
-authent_unit_test(Mod) ->
-    {setup,
-        fun() ->
-            Init = list_to_atom("init_"++Mod),
-            ?MODULE:Init(),
-
-            meck:new(epbxd_hooks),
-            meck:expect(epbxd_hooks, add,
-                fun(Key,Prio,{Mod2,Fun2}, GOpts) ->
-                    ok
-                end
-            ),
-            meck:expect(epbxd_hooks, del,
-                fun(Key, {Mod3,Fun3}) ->
-                    ok
-                end
-            ),
-
-            list_to_atom("mod_authent_"++Mod)
-        end,
-        fun(_) ->
-            meck:unload(epbxd_hooks),
-
-            Down = list_to_atom("down_"++Mod),
-            ?MODULE:Down()
-        end,
-        fun(Mod2) ->
-            [
-                {"loading module", 
-                    ?_assertEqual(ok, Mod2:start([]))
-                },
-
-                {"authent: user found", 
-                    ?_assertEqual({stop, {endpoints,"bob"}}, Mod2:authent({authent,0}, {"bob",nop,nop}, [], []))
-                },
-
-                {"authent: user not found", 
-                    ?_assertEqual({next, [state]}, Mod2:authent({authent,0}, {"alice",nop,nop}, [state], []))
-                },
-
-                {"unloading module", 
-                    ?_assertEqual(ok, Mod2:stop())
-                }
-            ]
-        end
-    }.
 
 mod_authent_internal_test_() ->
     {setup, local,
@@ -71,7 +25,9 @@ mod_authent_internal_test_() ->
         end,
         fun(_) ->
             [
-                {"Internal authentication (mnesia)", authent_unit_test("internal")}
+                {"Internal based authentication (mnesia)", 
+                    test_authent_generic:authent_unit_test(?MODULE, "internal", [])
+                }
             ]
         end
     }.
